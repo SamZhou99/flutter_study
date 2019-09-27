799c0460e4e2f3cf87f57a3704182788b75cf9bc
SamZhou99 <45636987@qq.com>
2019-09-27 11:00:00 +0800

    update

---
+++ hello2/lib/data/list.dart
@@ -0,0 +1,41 @@
+class ListData {
+  final int id;
+  final String title;
+  final String type1;
+  final String type2;
+  final String img;
+  final String link;
+  final String sourceSite;
+  final String video;
+  final int status;
+  final int isHtml;
+  final String date;
+  final String created;
+
+  ListData(
+      this.id,
+      this.title,
+      this.type1,
+      this.type2,
+      this.img,
+      this.link,
+      this.sourceSite,
+      this.video,
+      this.status,
+      this.isHtml,
+      this.date,
+      this.created);
+}
+
+// "id": 56293,
+// "title": "妊娠OKメイドと子作りSEX 浅田結梨",
+// "type1": "美少女",
+// "type2": "美少女",
+// "img": "https://tu.91zytu.com/images/2019/09/26/msn4122.jpg",
+// "link": "/show-9233.html",
+// "source_site": "35zy1",
+// "video": "https://www.800-cdn.com/20190924/Ni0GQMLP/index.m3u8",
+// "status": 1,
+// "is_html": 1,
+// "date": "2019-09-26T00:00:00.000Z",
+// "created": "2019-09-26T05:00:05.000Z"
