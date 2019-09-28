62a39ab70257528fbae80a7fee157994dc219bd5
sam.z <45636987@qq.com>
2019-09-28 15:46:45 +0800

    update

--- hello2/lib/main.dart
+++ hello2/lib/main.dart
@@ -5,73 +5,24 @@ class TabbedAppBarSample extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: new DefaultTabController(
-        length: choices.length,
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('Tabbed AppBar'),
-            bottom: new TabBar(
-              isScrollable: true,
-              tabs: choices.map((Choice choice) {
-                return new Tab(
-                  text: choice.title,
-                  icon: new Icon(choice.icon),
-                );
-              }).toList(),
+        home: Scaffold(
+          body: Center(
+            child: Container(
+              child: Text('我是一段 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 很长 的文字'),
+              // alignment: Alignment.center,
+              alignment: Alignment.topLeft,
+              width: 300.0,
+              height: 300.0,
+              color: Colors.amber,
+              padding: const EdgeInsets.all(10.0),
             ),
           ),
-          body: new TabBarView(
-            children: choices.map((Choice choice) {
-              return new Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new ChoiceCard(choice: choice),
-              );
-            }).toList(),
-          ),
-        ),
-      ),
-    );
+        )
+      );
   }
 }
 
-class Choice {
-  const Choice({ this.title, this.icon });
-  final String title;
-  final IconData icon;
-}
 
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'CAR', icon: Icons.directions_car),
-  const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
-  const Choice(title: 'BOAT', icon: Icons.directions_boat),
-  const Choice(title: 'BUS', icon: Icons.directions_bus),
-  const Choice(title: 'TRAIN', icon: Icons.directions_railway),
-  const Choice(title: 'WALK', icon: Icons.directions_walk),
-];
-
-class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({ Key key, this.choice }) : super(key: key);
-
-  final Choice choice;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return new Card(
-      color: Colors.white,
-      child: new Center(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Icon(choice.icon, size: 128.0, color: textStyle.color),
-            new Text(choice.title, style: textStyle),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 void main() {
   runApp(new TabbedAppBarSample());
