2d3337863c141c75eae947c068bfdefaf772bb32
sam.z <45636987@qq.com>
2021-09-21 21:50:37 +0800

    update

--- hello2/lib/data/list.dart
+++ hello2/lib/data/list.dart
@@ -27,15 +27,3 @@ class ListData {
       this.created);
 }
 
-// "id": 56293,
-// "title": "妊娠OKメイドと子作りSEX 浅田結梨",
-// "type1": "美少女",
-// "type2": "美少女",
-// "img": "https://tu.91zytu.com/images/2019/09/26/msn4122.jpg",
-// "link": "/show-9233.html",
-// "source_site": "35zy1",
-// "video": "https://www.800-cdn.com/20190924/Ni0GQMLP/index.m3u8",
-// "status": 1,
-// "is_html": 1,
-// "date": "2019-09-26T00:00:00.000Z",
-// "created": "2019-09-26T05:00:05.000Z"
